e5b39cfdf7a2faf3f3d93fbc9df25b5b823426c5
jayd7025 <jayd7025@gmail.com>
2024-03-28 16:16:20 -0700

    Initial Commit

---
+++ EssentialFeedV2/EssentialFeedV2.h
@@ -0,0 +1,18 @@
+//
+//  EssentialFeedV2.h
+//  EssentialFeedV2
+//
+//  Created by Jaavion Davis on 3/28/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EssentialFeedV2.
+FOUNDATION_EXPORT double EssentialFeedV2VersionNumber;
+
+//! Project version string for EssentialFeedV2.
+FOUNDATION_EXPORT const unsigned char EssentialFeedV2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EssentialFeedV2/PublicHeader.h>
+
+
